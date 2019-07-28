e22b09bfc9ef77b8fd8ac98df5a8afc81cc35c4f
Mukesh Kumar Gupta <mukesh_gupta85@yahoo.com>
2019-07-28 17:37:04 +0530

    started basic use of python

---
+++ python-tutorials.py
@@ -0,0 +1,107 @@
+import os  #for pause the script
+
+#####################################################################################
+#Lists are mutable
+# Declaring a list 
+L = [1, "a" , "string" , 1+2] 
+print L 
+L.append(6) 
+print L 
+L.pop() 
+print L
+#Output
+#[1, 'a', 'string', 3]
+#[1, 'a', 'string', 3, 6]
+#[1, 'a', 'string', 3]
+
+#tuple is a sequence of immutable
+tup = (1, "a", "string", 1+2) 
+print tup 
+print tup[1] 
+#Output
+#(1, 'a', 'string', 3)
+#a
+
+s = "Hello World"
+for i in s : 
+    print i 
+
+#Output ach character in nw line
+
+L = [1, 4, 5, 7, 8, 9] 
+for  i in L: 
+    print i, 
+	
+#output
+#1 4 5 7 8 9
+
+for  i in range(0, 10): 
+    print i,
+#output	
+#output	0 1 2 3 4 5 6 7 8 9
+#####################################################################################
+
+##eference: https://stackoverflow.com/questions/11552320/correct-way-to-pause-python-program
+##Make delay for some time
+#import time
+#print("something")
+#time.sleep(5.5)    # pause 5.5 seconds
+#print("something")
+
+#####################################################################################
+print ('\n')
+##Function definition
+##Reference: https://www.techbeamers.com/understand-python-comment-docstring/
+# Define a list of months
+print "Function use"
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec']
+
+# Function to print the calender months
+def showCalender(months):
+    # For loop that traverses the list and prints the name of each month
+    for month in months:
+        print(month)
+
+showCalender(months)
+
+##In triple quotes u can write many numbr of line : Rference: https://www.techbeamers.com/understand-python-comment-docstring/
+##Note: Multiline comments don't actually exist in Python
+def theFunction():
+    '''
+This function demonstrate the use of docstring in Python.
+This function demonstrate the use of docstring in Python1.
+    '''
+    print("Python docstrings are not comments.")
+
+print("\nJust printing the docstring value...")
+##print(theFunction.__doc__)    ## uncomments when it is required
+#####################################################################################
+
+#####################################################################################
+##Modular programming in python
+##Reference: https://www.python-course.eu/modules_and_modular_programming.php
+## A lot of example is given here so u hav to see all the example
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+def ifib(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+	
+##Note: We can import this module in the interactive python shell and call the functions by prefixing them with "fibonacci.":
+print(fib(30))
+#Output: 832040
+print(ifib(100))
+ifibAlias = ifib
+print(ifibAlias(100))
+#Output: 354224848179261915075L
+#####################################################################################
+
+os.system("pause")
